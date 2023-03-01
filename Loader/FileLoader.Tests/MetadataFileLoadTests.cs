99f86afefe29192a1fc9be2898b495dd42a4f1ca
Mirko Sekulic <misha.sekulic@gmail.com>
2023-03-01 14:59:16 +0100

    run pipeline

--- Loader/FileLoader.Tests/MetadataFileLoadTests.cs
+++ Loader/FileLoader.Tests/MetadataFileLoadTests.cs
@@ -5,6 +5,7 @@
         [Theory]
         [InlineData("Model/Metadata/example.json")]
         [InlineData("Model/Metadata/example.metadata.json")] 
+        [InlineData("Model/Metadata/new.metadata.json")] 
         public void EmbeddedFile_ShouldExist(string filePath)
         {
             string file = ResourceLoaderHelper.LoadTestDataAsString(filePath);
